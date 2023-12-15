0e70f4019ff1331b1479b6b97067fca7c9ac0217
Divesh <kumardiv151@gmail.com>
2023-12-16 02:27:01 +0530

    improve client part

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import './App.css'
+import Example from "./components/Example";
 
 function App() {
 
   return (
    <>
-   <p className='text-3xl font-bold underline '>hyy</p>
+   <Routes>
+      <Route path="/" element={ <Example /> } />
+   </Routes>
+
+   <p>hello</p>
    </>
   )
 }
---
+++ client/src/components/Example.tsx
@@ -0,0 +1,4 @@
+
+export default function example() {
+  return <div>example</div>;
+}
